5bc6d60509fadbdedc091ec168e204f46680dfad
Tristan Rimmer <tristan2rimmer05@gmail.com>
2025-05-16 10:17:38 +0100

    Added spherical coordinates to complete raybounce method

--- src/raylogic/raylogic.cc
+++ src/raylogic/raylogic.cc
@@ -3,42 +3,20 @@
 #include "vectors/vector_advanced.hh"
 #include "vectors/vector_definitions.hh"
 #include "vectors/vector_methods.hh"
+#include "vectors/vector_overloads.hh"
 
 #include <random>
 
 using Colours::BasicColour;
 using Vectors::Line;
+using Vectors::operator+;
 
+// TODO: generate a program wide rand gen and device and pass it by ref
 auto RayLogic::calculate_new_ray_direction(const Line<3, double> &ray,
                                            const Vec<3, double> &p_of_i,
                                            const Vec<3, double> &normal,
                                            const BasicColour &object_colour)
     -> Line<3, double> {
-  /*
-   To calculate effective directions, the method will do the following:
-   - Calculate the direction of the perfectly specular bounce direction
-   - Calculate the acute angle between that and the normal
-   - Calculate the acute angle between that and the common horizontal between
-       the new specular bounce and the normal (this is already calculated when
-       getting the new specular bounce), or just pi/2 - previous angle
-   - It will then using the specularilty parameter as a % to generate a random
-     offset represented by two angles
-   - One angle will be the same plane which the angle between the normal and
-   the bounce is in
-   - The other angle is perpendicular to this
-   - The angle range for Angle1 is pi * (1-specularily), which is randomly
-       chosen.
-   - Once the angle is chosen, it is rotated backwards by the angle between
-       the bounce and the normal which will ensure that the bounce is not
-       going back into the shape
-   - The same thing is done for the perpendicular angle, however there is
-       no angle rotation necessary
-
-   - The two vectors are added together and finally added to the bounce
-       direction before finally being normalised
-
-  */
-
   // To get the perfectly specular bounce direction, cross the ray direction
   // with the normal, and then cross the ray direction with that result.
   // Lastly, dot it with the normal and if the result is negative then invert
@@ -46,6 +24,8 @@ auto RayLogic::calculate_new_ray_direction(const Line<3, double> &ray,
   //
   // First confirm that the normal is the correct direction
   auto normal_clone = normal;
+  Vectors::normalise(normal_clone);
+
   auto ray_normal_dot = Vectors::dot(ray.second, normal_clone);
 
   if (ray_normal_dot >= 0) {
@@ -74,7 +54,8 @@ auto RayLogic::calculate_new_ray_direction(const Line<3, double> &ray,
 
   // RNG things
   auto obj_col_specularity = object_colour[7];
-  auto rand_gen = std::mt19937();
+  std::random_device randomised_seed;
+  auto rand_gen = std::mt19937(randomised_seed());
   auto bounce_rand_range = PI * (1 - obj_col_specularity);
   // TODO: look for a cosine weighted distribution
 
@@ -84,15 +65,25 @@ auto RayLogic::calculate_new_ray_direction(const Line<3, double> &ray,
   // This is the angle which a vector should be formed from which lies in the
   // same plane as the normal clone, incoming bounce and outgoing bounce
   auto spec_norm_angle = spec_distribution(rand_gen) - angle_offset;
+  auto perpendicular_angle = spec_distribution(rand_gen);
 
-  // To actually calculate that vector:
-  // - idea one, 2d polar coordinates which you then project into 3d?
-  // - idea two, some kind of simultaneous equation involving a Mod(cross) and
-  //     dot product
-  // - Spherical Polar Coordinates
-  // - Form a plane which contains the incoming ray angle, the normal and the
-  //     perfect specular bounce
-  // - Simultaneous equations of dot product matching what you want, and that
-  //     resolves the normal form of the EQ for a plane
-  // TODO: FIGURE IT OUT
+  // Normalised versions of the 3 local axis for Spherical coordinates
+  Vectors::normalise(horizontal);
+  Vectors::normalise(ray_normal_cross);
+  Vectors::normalise(specular_bounce_dir);
+
+  auto actual_bounce_dir =                            //
+      Vectors::scale(specular_bounce_dir,             //
+                     std::cos(perpendicular_angle)) + //
+      Vectors::scale(horizontal,                      //
+                     std::sin(perpendicular_angle) *  //
+                         std::cos(spec_norm_angle)) + //
+      Vectors::scale(ray_normal_cross,                //
+                     std::sin(perpendicular_angle) *  //
+                         std::sin(spec_norm_angle));  //
+
+  Vectors::normalise(actual_bounce_dir);
+
+  return Line<3, double>(
+      std::make_pair(ray.first + normal_clone, actual_bounce_dir));
 }
