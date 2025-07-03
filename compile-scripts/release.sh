b45cd70fdd1957d603a3c9bc51a57b82e75943b8
Tristan Rimmer <tristan.rimmer@stfc.ac.uk>
2025-07-03 15:03:08 +0100

    Replaced /compile-scripts/release to work on Fedora

--- compile-scripts/release.sh
+++ compile-scripts/release.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "============== Compiling Optimised Build =============="
-meson setup --wipe build --buildtype=release -Dcpp_args='-O3 -lGL'
+meson setup --wipe build --buildtype="release" -Dcpp_args="-O3"
 meson compile -C build
 
